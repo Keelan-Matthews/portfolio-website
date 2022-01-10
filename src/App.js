e20d58048ee1a264f7285ff2b91176ebd6f27260
Keelan <keelanmatthews123@gmail.com>
2022-01-10 16:55:30 +0200

    Contact complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Header } from './components/Header';
 import { AboutMe } from './components/AboutMe';
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
+import { Contact } from './components/Contact';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <AboutMe />
       <Skills />
       <Projects />
+      <Contact />
     </main>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,10 @@ import React from 'react';
 export const Contact = () => {
     return (
         <section className="contact-section">
-            
+            <h2>Have any questions?</h2>
+            <a href="mailto:keelanmatthews123@gmail.com">
+                <div>Let's talk</div>
+            </a>
         </section>
     )
 }
