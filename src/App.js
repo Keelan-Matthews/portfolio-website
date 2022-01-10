9b37de46a227aa3205f5c7cb23899485845759a0
Keelan <keelanmatthews123@gmail.com>
2022-01-10 16:43:38 +0200

    re-commit projects

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { NavBar } from './components/NavBar';
 import { Header } from './components/Header';
 import { AboutMe } from './components/AboutMe';
 import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <AboutMe />
       <Skills />
+      <Projects />
     </main>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Contact = () => {
+    return (
+        <section className="contact-section">
+            
+        </section>
+    )
+}
