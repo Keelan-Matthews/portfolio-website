00694ee9696db978b403ab47563325d2e559fcb7
Keelan <keelanmatthews123@gmail.com>
2022-01-11 13:26:04 +0200

    More responsive, nav bar complete responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export const NavBar = () => {
+
+    const [showLinks, setShowLinks] = useState(false);
+
     return (
         <nav className="nav-bar">
-            <ul className="navigation">
+            <div className="menu-icon" onClick={() => setShowLinks(!showLinks)}>
+                <FontAwesomeIcon icon={ faBars } />
+            </div>
+            <ul className="navigation" id={ showLinks ? "show-links" : "" }>
                 <li><a href="#about-me-nav">About</a></li>
                 <li><a href="#skills-nav">Skills</a></li>
                 <li><a href="#projects-nav">Projects</a></li>
@@ -22,7 +29,7 @@ export const NavBar = () => {
                     <FontAwesomeIcon icon={ faLinkedin } style={{ width: "24px", height: "24px", margin: "10px"}} />
                     <p>LinkedIn</p>
                 </a></li>
-                <li><a href="mailto:keelanmatthews123@gmail.com" target="_blank">
+                <li><a href="mailto:keelanmatthews123@gmail.com" target="_blank" className="mail-button">
                     <div className="email-icon">
                         <FontAwesomeIcon icon={ faEnvelope } />
                     </div>
