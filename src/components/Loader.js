a30bad1e5b3ab689b5136a4247002f87559dc2cb
Keelan <keelanmatthews123@gmail.com>
2022-01-12 19:50:02 +0200

    Reran build

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,16 +1,16 @@
-// import React from 'react';
-// import { BoxesLoader } from "react-awesome-loaders";
+import React from 'react';
+import { BoxesLoader } from "react-awesome-loaders";
 
-// export const Loader = () => {
-//     return (
-//         <div className="loader">
-//            <BoxesLoader
-//                 boxColor={"#47D2A0"}
-//                 shadowColor={"#25262A"}
-//                 style={{ marginBottom: "20px" }}
-//                 desktopSize={"128px"}
-//                 mobileSize={"80px"}
-//             /> 
-//         </div>
-//     )
-// }
+export const Loader = () => {
+    return (
+        <div className="loader">
+           <BoxesLoader
+                boxColor={"#47D2A0"}
+                shadowColor={"#25262A"}
+                style={{ marginBottom: "20px" }}
+                desktopSize={"128px"}
+                mobileSize={"80px"}
+            /> 
+        </div>
+    )
+}
