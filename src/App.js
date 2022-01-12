cb2bbb90bdbaafcfdc09f35eaa2a22b31d24c2c3
Keelan <keelanmatthews123@gmail.com>
2022-01-12 19:56:35 +0200

    Uninstalled awesome loaders

--- src/App.js
+++ src/App.js
@@ -6,35 +6,28 @@ import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
-import { Loader } from './components/Loader';
-import { useState, useEffect } from 'react';
+// import { Loader } from './components/Loader';
+// import { useState, useEffect } from 'react';
 
 function App() {
-  const [loading, setLoading] = useState(false);
+  // const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    setLoading(true)
-    setTimeout(() => {
-      setLoading(false)
-    }, 4000)
-  }, [])
+  // useEffect(() => {
+  //   setLoading(true)
+  //   setTimeout(() => {
+  //     setLoading(false)
+  //   }, 4000)
+  // }, [])
 
   return (
     <main>
-      {
-        loading ?
-          <Loader />
-        :
-          <div>
-            <NavBar />
-            <Header />
-            <AboutMe />
-            <Skills />
-            <Projects />
-            <Contact />
-            <Footer />
-          </div>
-      }
+        <NavBar />
+        <Header />
+        <AboutMe />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer />
     </main>
   );
 }
