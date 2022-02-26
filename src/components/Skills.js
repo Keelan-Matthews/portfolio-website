df627c06349360a9b1745206b22bac0e620a2b6a
Keelan <keelanmatthews123@gmail.com>
2022-02-26 13:37:47 +0200

    Added skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -10,6 +10,16 @@ import psLogo from '../media/svg/ps-logo.svg';
 import SassLogo from '../media/svg/sass-logo.svg';
 import FigmaLogo from '../media/svg/figma-logo.svg';
 import ReactIcon from '../media/svg/react-logo.svg';
+import Resolve from '../media/svg/resolve-logo.svg';
+import Bootstrap from '../media/svg/bootstrap-logo.svg';
+import Angular from '../media/svg/angular-logo.svg';
+import Java from '../media/svg/java-logo.svg';
+import Git from '../media/svg/git-logo.svg';
+import Php from '../media/svg/php-logo.svg';
+import Max from '../media/svg/3dsmax-logo.svg';
+import Mongo from '../media/svg/mongodb-logo.svg';
+import Vue from '../media/svg/vue-logo.svg';
+import Mysql from '../media/svg/mysql-logo.svg';
 
 export const Skills = () => {
     return (
@@ -29,13 +39,29 @@ export const Skills = () => {
                     <SkillCard vector={ Css } alt="CSS logo" name="CSS" />
                     <SkillCard vector={ jsLogo } alt="JavaScript logo" name="JavaScript" />
                     <SkillCard vector={ SassLogo } alt="Sass logo" name="SASS" />
-                    <SkillCard vector={ ReactIcon } alt="ReactJS logo" name="ReactJS" />
-                    <SkillCard vector={ C } alt="C++ logo" name="C++" />
+                    <SkillCard vector={ Bootstrap } alt="Sass logo" name="Bootstrap" />
+                    <SkillCard vector={ ReactIcon } alt="ReactJS logo" name="React" />
+                    <SkillCard vector={ Angular } alt="Sass logo" name="Angular" />
+                    <SkillCard vector={ Vue } alt="Blender logo" name="Vue" />
+                </div>
+                <div className="skill-container">
                     <SkillCard vector={ aiLogo } alt="Adobe Illustrator logo" name="Adobe Illustrator" />
                     <SkillCard vector={ psLogo } alt="Adobe Photoshop logo" name="Adobe Photoshop" />
                     <SkillCard vector={ FigmaLogo } alt="Figma logo" name="Figma" />
                     <SkillCard vector={ Blender } alt="Blender logo" name="Blender" />
-                </div>
+                    <SkillCard vector={ Max } alt="Blender logo" name="3Ds Max" />
+                    <SkillCard vector={ Resolve } alt="Blender logo" name="Da Vinci Resolve" />
+                </div>  
+                <div className="skill-container">
+                    <SkillCard vector={ Mongo } alt="Blender logo" name="Mongo DB" />
+                    <SkillCard vector={ Mysql } alt="Blender logo" name="MySQL" />
+                    <SkillCard vector={ Php } alt="Sass logo" name="PHP" />
+                    <SkillCard vector={ Git } alt="Sass logo" name="Git" />
+                </div>  
+                <div className="skill-container">
+                    <SkillCard vector={ C } alt="C++ logo" name="C++" />
+                    <SkillCard vector={ Java } alt="Sass logo" name="Java" />   
+                </div>   
             </div>
 
             <div className="skills-background-circles">    
